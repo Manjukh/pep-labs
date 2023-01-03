3e248749c8a44b03d48667353a4b082798778cb1
Manjukh <manjukasthuri@yahoo.com>
2023-01-03 17:14:27 +0000

    Saved progress on ThrowCustomException 2023-01-03 17:14:27 +0000

--- UncheckedException/src/main/java/CauseUncheckedException.java
+++ UncheckedException/src/main/java/CauseUncheckedException.java
@@ -14,6 +14,8 @@ public class CauseUncheckedException {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
+        int[] arr= {1,2,3,4,5};
+        System.out.println(arr[5]);
 
     }
 }

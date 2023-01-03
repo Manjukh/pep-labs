6d3da9779b7449c49d648926492468b98d07f062
Manjukh <manjukasthuri@yahoo.com>
2023-01-03 21:08:25 +0000

    Saved progress on ThrowCustomException 2023-01-03 21:08:25 +0000

--- ThrowCustomException/src/main/java/CustomException.java
+++ ThrowCustomException/src/main/java/CustomException.java
@@ -6,7 +6,6 @@
  */
 public class CustomException extends Exception{
 
-    public CustomException(String string) {
-        super(string);
-    }
+    
+    
 }
--- ThrowCustomException/src/main/java/CustomExceptionThrower.java
+++ ThrowCustomException/src/main/java/CustomExceptionThrower.java
@@ -9,14 +9,9 @@ public class CustomExceptionThrower {
      * I recommend looking at the test case for a good example of try/catch block usage, as well.
      */
     public void throwCustomException(){
-        try{ 
-            throw new CustomException("Exception Caught");
-        }
         
-            catch(CustomException e) {
-            System.out.println(e);
-
-        }
+        
+           
     }
-
 }
+

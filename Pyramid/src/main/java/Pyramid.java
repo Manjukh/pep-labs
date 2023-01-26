e153153b517969b7cd88f8b37687f91c17fb4c54
Manjukh <manjukasthuri@yahoo.com>
2023-01-26 21:25:08 +0000

    Saved progress on Library 2023-01-26 21:25:08 +0000

--- Pyramid/src/main/java/Pyramid.java
+++ Pyramid/src/main/java/Pyramid.java
@@ -27,17 +27,15 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
-       String ast="*";
-        for(int i=0; i<n; i++){
-
-            for(int j=0;j<=i; j++){
-                
-               System.out.print(ast);
-            }
-            System.out.print(" ");
-        }
-       return " ";
+        String ast="*";
+        String out = "";
+  for (int i = 0; i < n; i++) {
+    out += ast;
+    System.out.println(out);
+  }
+  return out;
+    }
     }
    
-}
+
 

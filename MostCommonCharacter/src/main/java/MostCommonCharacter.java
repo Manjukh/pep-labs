4ca2dbb41410b79e444c2f805db7f60b573073fd
Manjukh <manjukasthuri@yahoo.com>
2023-02-26 04:34:21 +0000

    Saved progress on MostCommonCharacter 2023-02-26 04:34:21 +0000

--- MostCommonCharacter/src/main/java/MostCommonCharacter.java
+++ MostCommonCharacter/src/main/java/MostCommonCharacter.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class MostCommonCharacter {
     /**
@@ -8,7 +11,47 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        Hash
-        return ' ';
+        HashMap<Character, Integer> charCountMap = new HashMap<>();
+         
+        char[] charArray = str.replaceAll("\\s+", "").toCharArray();
+         
+        for (char c : charArray) 
+        {
+            if (charCountMap.containsKey(c))
+            {
+                charCountMap.put(c, charCountMap.get(c)+1);
+            }
+            else
+            {
+                charCountMap.put(c, 1);
+            }
+        }
+         
+        Set<Entry<Character, Integer>> entrySet = charCountMap.entrySet();
+         
+        int maxCount = 0;
+         
+        char maxChar = 0;
+         
+        for (Entry<Character, Integer> entry : entrySet) 
+        {
+            if (entry.getValue() > maxCount)
+            {
+                maxCount = entry.getValue();
+                 
+                maxChar = entry.getKey();
+            }
+        }
+         
+       // System.out.println("Input String : "+inputString);
+         
+        System.out.println("Maximum Occurring char and its count :");
+         
+        System.out.println(maxChar+" : "+maxCount);
+    
+     
+    return maxChar;
+    
+    
     }
 }
